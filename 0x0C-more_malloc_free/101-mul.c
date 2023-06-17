a74a39fae82e61631393f4a5e33e35fb195798a0
IlesanmiEster <hadassah4u2nv@gmail.com>
2023-06-17 06:05:03 -0700

    Updated

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -5,10 +5,10 @@
 #define ERR_MSG "Error"
 
 /**
- * is_digit - checks if a string contains a non-digit char
+ * is_digit - check if a string contains a non-digit char
  * @s: string to be evaluated
  *
- * Return: 0 if a non-digit is found, 1 otherwise
+ * Return: 0 if a non-digit is found, otherwise return 1
  */
 int is_digit(char *s)
 {
